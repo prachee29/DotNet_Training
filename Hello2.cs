2fae961af49e484f5abfc939a2a2dc6c2bb93bbd
Prachee Shukla <shuklaprachee03@gmail.com>
2024-12-09 21:48:38 +0530

    New hello

---
+++ Hello2.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace learn_dotnet.practice.DotNet_Training
+{
+	public class Hello2
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("HelloWorld2");
+		}
+	}
+}
+
